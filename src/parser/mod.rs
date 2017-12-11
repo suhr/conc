562ca205ce65e9098d83011432a94c4c8f5f5e07
Сухарик <suhr@i2pmail.org>
2017-12-11 18:20:38 +0300

    Rewrite some parsing code

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -41,6 +41,17 @@ enum Simple {
     Float(String, Position)
 }
 
+fn into_simple(tok: Token) -> Result<Simple, Token> {
+    match tok.lexeme {
+        Lexeme::Word(w) => Ok(Simple::Word(w, tok.position)),
+        Lexeme::Integer(i) => Ok(Simple::Integer(i, tok.position)),
+        Lexeme::Float(f) => Ok(Simple::Float(f, tok.position)),
+        Lexeme::String(s) =>Ok(Simple::String(s, tok.position)),
+        Lexeme::Char(c) => Ok(Simple::Char(c, tok.position)),
+        _ => Err(tok),
+    }
+}
+
 #[derive(Debug)]
 struct StackEff {
     input: Vec<Name>,
@@ -53,7 +64,20 @@ enum Symbol {
     FullExpr(FullExpr),
 }
 
+impl Symbol {
+    fn is_lexeme(&self, lex: Lexeme) -> bool {
+        self.ref_lexeme() == Some(&lex)
+    }
 
+    fn ref_lexeme(&self) -> Option<&Lexeme> {
+        match self {
+            &Symbol::Token(ref tok) =>
+                Some(&tok.lexeme),
+            _ =>
+                None,
+        }
+    }
+}
 
 #[derive(Debug)]
 pub enum ParserError {
@@ -69,165 +93,100 @@ impl ::std::convert::From<LexerError> for ParserError {
 }
 
 #[derive(Debug)]
-pub struct Parser<'a, R: 'a + io::Read> {
+pub struct Parser<'a, R: 'a + io::Read, T> {
     lexer: &'a mut Lexer<R>,
-    stack: Vec<Symbol>,
-    cursor: (Token, Token),
+    cursor: Token,
+    input: Vec<(Symbol, usize)>,
+    output: Vec<T>,
 }
 
-impl<'a, R: io::Read> Parser<'a, R> {
+impl<'a, R: io::Read, T> Parser<'a, R, T> {
     pub fn new(lexer: &'a mut Lexer<R>) -> Result<Self, ParserError> {
         let tok = lexer.next_token()?;
-        let la = lexer.next_token()?;
 
         Ok(Parser {
             lexer,
-            stack: vec![],
-            cursor: (tok, la),
+            cursor: tok,
+            input: vec![],
+            output: vec![],
         })
     }
 
     fn shift_lexer(&mut self) -> Result<Token, ParserError> {
         let mut tok = self.lexer.next_token()?;
-        ::std::mem::swap(&mut tok, &mut self.cursor.1);
-        ::std::mem::swap(&mut tok, &mut self.cursor.0);
+        ::std::mem::swap(&mut tok, &mut self.cursor);
 
         Ok(tok)
     }
 
     fn shift(&mut self) -> Result<(), ParserError> {
         let tok = self.shift_lexer()?;
-        self.stack.push(Symbol::Token(tok));
+        self.input.push((Symbol::Token(tok), self.output.len()));
 
         Ok(())
     }
 
-    fn terminate(&mut self) -> Option<Symbol> {
-        if self.stack.len() == 1 {
-            self.stack.pop()
+    fn last_input_pos(&self) -> Option<usize> {
+        Some(self.input.last()?.1)
+    }
+
+    fn terminate(&mut self) -> Option<T> {
+        if self.input.is_empty() && self.output.len() == 1 {
+            self.output.pop()
         } else {
             None
         }
     }
 }
 
-fn into_simple(tok: Token) -> Result<Simple, Token> {
-    match tok.lexeme {
-        Lexeme::Word(w) => Ok(Simple::Word(w, tok.position)),
-        Lexeme::Integer(i) => Ok(Simple::Integer(i, tok.position)),
-        Lexeme::Float(f) => Ok(Simple::Float(f, tok.position)),
-        Lexeme::String(s) =>Ok(Simple::String(s, tok.position)),
-        Lexeme::Char(c) => Ok(Simple::Char(c, tok.position)),
-        _ => Err(tok),
-    }
-}
+fn pop_conc<R: io::Read, T>(parser: &mut Parser<R, T>) -> Option<(T, T)> {
+    if parser.output.len() < 2 { return None }
 
-fn symbol_token_lexeme(sym: &Symbol, lex: Lexeme) -> bool {
-    match sym {
-        &Symbol::Token(ref t) =>
-            t.lexeme == lex,
-        _ => false,
-    }
-}
+    let cond =
+        parser.input.last()?.0.is_lexeme(Lexeme::Comma)
+        && parser.input.last()?.1 < parser.output.len();
+    if cond {
+        drop(parser.input.pop());
 
-fn pop_token(stack: &mut Vec<Symbol>) -> Option<Token> {
-    let v = stack.pop();
+        let e2 = parser.output.pop().unwrap();
+        let e1 = parser.output.pop().unwrap();
 
-    match v {
-        Some(Symbol::Token(tok)) => Some(tok),
-        Some(sym) => {
-            stack.push(sym);
-            None
-        }
-        _ => None,
-    }
-}
-
-fn pop_simple(stack: &mut Vec<Symbol>) -> Option<Simple> {
-    let v = stack.pop();
-
-    match v {
-        Some(Symbol::FullExpr(FullExpr::Simple(s))) => Some(s),
-        Some(sym) => {
-            stack.push(sym);
-            None
-        }
-        _ => None,
-    }
-}
-
-fn pop_fullexpr(stack: &mut Vec<Symbol>) -> Option<FullExpr> {
-    let v = stack.pop();
-
-    match v {
-        Some(Symbol::FullExpr(e)) => Some(e),
-        Some(sym) => {
-            stack.push(sym);
-            None
-        }
-        _ => None,
-    }
-}
-
-fn pop_lexeme(stack: &mut Vec<Symbol>, lex: Lexeme) -> Option<Token> {
-    let tok = pop_token(stack)?;
-
-    if tok.lexeme == lex {
-        Some(tok)
+        Some((e1, e2))
     } else {
-        stack.push(Symbol::Token(tok));
         None
     }
 }
 
-fn pop_conc(stack: &mut Vec<Symbol>) -> Option<(FullExpr, FullExpr)> {
-    let e2 = pop_fullexpr(stack)?;
+fn pop_comp<R: io::Read, T>(parser: &mut Parser<R, T>) -> Option<(T, T)> {
+    if parser.output.len() < 2 { return None }
 
-    let comma = match pop_lexeme(stack, Lexeme::Comma) {
-        Some(c) => c,
-        None => {
-            stack.push(Symbol::FullExpr(e2));
-            return None
+    let cond = parser.last_input_pos().map(|p| p + 2 <= parser.output.len()).unwrap_or(true);
+    if !cond { return None }
+
+    match parser.cursor.lexeme {
+        Lexeme::Comma | Lexeme::Operator(_) => {
+            None
         },
-    };
+        _ => {
+            let e2 = parser.output.pop().unwrap();
+            let e1 = parser.output.pop().unwrap();
 
-    let e1 = match pop_fullexpr(stack) {
-        Some(e) => e,
-        None => {
-            stack.push(Symbol::Token(comma));
-            stack.push(Symbol::FullExpr(e2));
-            return None
+            Some((e1, e2))
         },
-    };
-
-    Some((e1, e2))
+    }
 }
 
-fn pop_comp(stack: &mut Vec<Symbol>) -> Option<(FullExpr, FullExpr)> {
-    let e2 = pop_fullexpr(stack)?;
-
-    let e1 = match pop_fullexpr(stack) {
-        Some(e) => e,
-        None => {
-            stack.push(Symbol::FullExpr(e2));
-            return None
-        },
-    };
-
-    Some((e1, e2))
-}
-
-fn parse_fullexpr<R: io::Read>(parser: &mut Parser<R>) -> Result<FullExpr, ParserError> {
+fn parse_fullexpr<R: io::Read>(parser: &mut Parser<R, FullExpr>) -> Result<FullExpr, ParserError> {
     loop {
         // reduce
         let cond =
-            reduce_simple(parser)
-            || reduce_conc(parser)
-            || reduce_comp(parser);
+            reduce_simple_fe(parser)
+            || reduce_conc_fe(parser)
+            || reduce_comp_fe(parser);
         if cond { continue }
 
         // shift
-        match parser.cursor.0.lexeme {
+        match parser.cursor.lexeme {
             Lexeme::Newline
             | Lexeme::Comment(_) =>
                 drop(parser.shift_lexer()?),
@@ -245,19 +204,12 @@ fn parse_fullexpr<R: io::Read>(parser: &mut Parser<R>) -> Result<FullExpr, Parse
         }
     }
 
-    match parser.terminate() {
-        Some(Symbol::FullExpr(fe)) => Ok(fe),
-        Some(s) => {
-            parser.stack.push(s);
-            Err(ParserError::Stuck)
-        },
-        None => Err(ParserError::Stuck),
-    }
+    parser.terminate().ok_or(ParserError::Stuck)
 }
 
-fn check_basic<R: io::Read>(parser: &mut Parser<R>) -> bool {
+fn check_basic<R: io::Read>(parser: &mut Parser<R, FullExpr>) -> bool {
     use self::Lexeme;
-    match parser.cursor.0.lexeme {
+    match parser.cursor.lexeme {
         Lexeme::Word(_)
         | Lexeme::Integer(_)
         | Lexeme::Float(_)
@@ -269,63 +221,63 @@ fn check_basic<R: io::Read>(parser: &mut Parser<R>) -> bool {
     }
 }
 
-fn reduce_conc<R: io::Read>(parser: &mut Parser<R>) -> bool {
-    if let Lexeme::Operator(_) = parser.cursor.0.lexeme {
-        return false
-    }
-
-    match pop_conc(&mut parser.stack) {
-        Some((FullExpr::Conc(mut conc), expr)) => {
-            conc.push(expr);
-            parser.stack.push(Symbol::FullExpr(FullExpr::Conc(conc)));
-            return true
-        },
-        Some((e1, e2)) => {
-            let conc = vec![e1, e2];
-            parser.stack.push(Symbol::FullExpr(FullExpr::Conc(conc)));
-            return true
-        },
-        _ => return false,
-    }
-}
-
-fn reduce_comp<R: io::Read>(parser: &mut Parser<R>) -> bool {
-    match parser.cursor.0.lexeme {
-        Lexeme::Operator(_)
-        | Lexeme::Comma =>
-            return false,
-        _ => (),
-    }
-
-    match pop_comp(&mut parser.stack) {
-        Some((FullExpr::Comp(mut comp), expr)) => {
-            comp.push(expr);
-            parser.stack.push(Symbol::FullExpr(FullExpr::Comp(comp)));
+fn reduce_conc_fe<R: io::Read>(parser: &mut Parser<R, FullExpr>) -> bool {
+    match pop_conc(parser) {
+        Some((FullExpr::Conc(mut conc), e2)) => {
+            conc.push(e2);
+            parser.output.push(FullExpr::Conc(conc));
             true
         },
         Some((e1, e2)) => {
-            let comp = vec![e1, e2];
-            parser.stack.push(Symbol::FullExpr(FullExpr::Comp(comp)));
+            parser.output.push(FullExpr::Conc(vec![e1, e2]));
             true
-        },
-        _ => false
-    }
-}
-
-fn reduce_simple<R: io::Read>(parser: &mut Parser<R>) -> bool {
-    if let Some(tok) = pop_token(&mut parser.stack) {
-        match into_simple(tok) {
-            Ok(s) => {
-                parser.stack.push(Symbol::FullExpr(FullExpr::Simple(s)));
-                true
-            }
-            Err(tok) => {
-                parser.stack.push(Symbol::Token(tok));
-                false
-            }
         }
-    } else {
-        false
+        _ =>
+            false,
+    }
+}
+
+fn reduce_comp_fe<R: io::Read>(parser: &mut Parser<R, FullExpr>) -> bool {
+    match pop_comp(parser) {
+        Some((FullExpr::Comp(mut comp), e2)) => {
+            comp.push(e2);
+            parser.output.push(FullExpr::Comp(comp));
+            true
+        },
+        Some((e1,e2)) => {
+            parser.output.push(FullExpr::Comp(vec![e1, e2]));
+            true
+        },
+        None =>
+            false,
+    }
+}
+
+fn reduce_enclosed_fe<R: io::Read>(parser: &mut Parser<R, FullExpr>) -> bool {
+    unimplemented!()
+}
+
+fn reduce_simple_fe<R: io::Read>(parser: &mut Parser<R, FullExpr>) -> bool {
+    let sym = parser.input.pop();
+    match sym {
+        Some((Symbol::Token(tok), n)) => {
+            match into_simple(tok) {
+                Ok(simple) => {
+                    parser.output.push(FullExpr::Simple(simple));
+                    true
+                },
+                Err(tok) => {
+                    parser.input.push((Symbol::Token(tok), n));
+                    false
+                },
+            }
+        },
+        Some(sym) => {
+            parser.input.push(sym);
+            false
+        }
+        _ =>
+            false,
     }
 }
 
@@ -333,7 +285,66 @@ fn reduce_simple<R: io::Read>(parser: &mut Parser<R>) -> bool {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use super::lexer;
+
+    fn format_indented(s: &str, indent: usize) -> String {
+        let mut string = String::new();
+        for _ in 0..indent {
+            string.push(' ')
+        }
+
+        format!("{}{}\n", string, s)
+    }
+
+    fn format_fullexpr(expr: &FullExpr, indent: usize) -> String {
+        match expr {
+            &FullExpr::Simple(Simple::String(ref s, _)) => {
+                let s = format!("String {:?}", s);
+                format_indented(&s, indent)
+            },
+            &FullExpr::Simple(Simple::Char(ref c, _)) => {
+                let s = format!("Char {:?}", c);
+                format_indented(&s, indent)
+            },
+            &FullExpr::Simple(Simple::Float(ref f, _)) => {
+                let s = format!("Float {:?}", f);
+                format_indented(&s, indent)
+            },
+            &FullExpr::Simple(Simple::Integer(ref i, _)) => {
+                let s = format!("Integer {:?}", i);
+                format_indented(&s, indent)
+            },
+            &FullExpr::Simple(Simple::Word(ref w, _)) => {
+                let s = format!("Word {:?}", w);
+                format_indented(&s, indent)
+            },
+            &FullExpr::Comp(ref comp) => {
+                let mut res = format_indented("Comp", indent);
+                for e in comp {
+                    let t = format_fullexpr(&e, indent + 2);
+                    res.push_str(&t);
+                }
+                res
+            },
+            &FullExpr::Conc(ref conc) => {
+                let mut res = format_indented("Conc", indent);
+                for e in conc {
+                    let t = format_fullexpr(&e, indent + 2);
+                    res.push_str(&t);
+                }
+                res
+            },
+            &FullExpr::Enclosed(ref e) => {
+                let mut res = format_indented("Enclosed", indent);
+                let t = format_fullexpr(&e, indent);
+                res.push_str(&t);
+                res
+            },
+            &FullExpr::Empty => {
+                format_indented("Empty", indent)
+            },
+            _ => unimplemented!(),
+        }
+    }
 
     #[test] fn abcdef_expr() {
         let expr = "a b,c d,e f\n".as_bytes();
@@ -342,7 +353,23 @@ mod tests {
         let mut parser = Parser::new(&mut lexer).unwrap();
         let expr = parse_fullexpr(&mut parser).unwrap();
 
-        assert_eq!(FullExpr::Empty, expr);
+        let pretty = format_fullexpr(&expr, 0);
+        let gold =
+    // Raw string litreals are painful
+r#"Comp
+  Word "a"
+  Conc
+    Word "b"
+    Word "c"
+  Conc
+    Word "d"
+    Word "e"
+  Word "f"
+"#;
+// Seriously,
+    // they are
+        // very painful
+        assert_eq!(gold, pretty);
     }
 
     // #[test] fn hello_expr() {
